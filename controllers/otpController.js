08014624f13dc9849471c4a04631ba975a865eaa
AbdulQohar1 <adebayoabdulqohar1@gmail.com>
2024-09-24 18:28:22 +0100

    Added otp controller and route functionalities

---
+++ controllers/otpController.js
@@ -0,0 +1,51 @@
+const otpGenerator = require('otp-generator');
+const OTP = require('../models/otp');
+const User = require('../models/user');
+
+exports.sendOTP = async (req, res) => {
+	try {
+		const { email } = req.body;
+
+		// confirm if user is already existing4
+		const checkUserPresent  = await User.findOne({ email });
+		// if user with the provided email already exist
+		if (checkUserPresent) {
+			return res.status(401).json({
+				success: false,
+				message: 'User is already registered!',
+			});
+		}
+
+		let otp = otpGenerator.generate(5, {
+			upperCaseAlphabets: false,
+			lowerCaseAlphabets: false,
+			specialChars: false,
+		});
+
+		let result = await OTP.findOne({ otp: otp });
+		while (result) {
+			otp = otpGenerator.generate(5, {
+				upperCaseAlphabets: false,
+			});
+			
+			result = await OTP.findOne({ otp: otp});
+		}
+
+		const otpPayload = { email, otp};
+		const otpBody = await OTP.create( otpPayload);
+
+		res.status(200).json({
+			success: true,
+			message: 'OTP sent successfully!', 
+			otp,
+		})
+
+	} catch (error) {
+		console.log(error.message);
+		return res.status(500).json({
+			success: false,
+			error: error.message
+		})
+		
+	}
+}
