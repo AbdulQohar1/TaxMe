d71c99e2eba79c0403246a3c5a98c256651ff384
AbdulQohar1 <adebayoabdulqohar1@gmail.com>
2025-01-03 22:27:11 +0100

    resolving uploader authentication error

--- controllers/uploadTaxDocument.js
+++ controllers/uploadTaxDocument.js
@@ -31,24 +31,26 @@ const uploadDocument = async (req , res) => {
       folder: 'documents'
     });
 
-    // const result = await cloudinary.uploader.upload(req.file.path, {
-    //   resource_type: 'raw',
-    //   // cloudinary folder name  
-    //   folder: 'documents' 
-    //   // specify resource type for documents
-    // });
-
     // respond with success and  file details
     return res.status(StatusCodes.OK).json({
       success: true,
       message: 'Tax document uploaded successfully!',
-      data: result
+      data: {
+        // Cloudinary URL
+        documentUrl: req.file.path,
+        // Public ID in Cloudinary
+        documentId: req.file.filename, 
+        originalName: req.file.originalname,
+        format: req.file.format,
+        size: req.file.size,
+        resourceType: req.file.resource_type
+      }
     });
   } catch (error) {
     console.log('Error uploading document: ', error);
     return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
       success: false,
-      message: 'Upload tax document failed.',
+      message: 'Document upload failed.',
       error: error.message
     })
     
@@ -56,38 +58,9 @@ const uploadDocument = async (req , res) => {
 }
 
 module.exports = {uploadDocument};
-// const uploadDocument = uploadTaxDocument.single('document'), 
-// function(req , res) {
-//   cloudinary.uploader.upload(req, file.name, function(err, result) {
-//     if (err) {
-//       console.log(err);
-//       return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
-//         success: false,
-//         message: 'Upload tax document failed...'
-//       })
-//     };
-
-//     res.status(StatusCodes.OK).json({
-//       success: true,
-//       message: 'Document uploaded successfully!',
-//       data: result
-//     })
-//   })
-// }
-
-// const uploadDocument = new CloudinaryStorage({
-//   cloudinary: cloudinary,
-//   params: {
-//     // Cloudinary folder name
-//     folder: 'documents', 
-//     // Force files to be saved as PDFs
-//     format: async (req, file) => 'pdf', 
-//     // Unique file name
-//     public_id: (req, file) => `${Date.now()}-${file.originalname}`,
-//     // For non-image files like PDFs, DOCX, etc. 
-//     resource_type: 'raw', 
-//   },
-// });
-
-
-
+ // const result = await cloudinary.uploader.upload(req.file.path, {
+    //   resource_type: 'raw',
+    //   // cloudinary folder name  
+    //   folder: 'documents' 
+    //   // specify resource type for documents
+    // });
