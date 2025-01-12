c6da0810de847623e700cf6f484374b242a6d752
AbdulQohar1 <adebayoabdulqohar1@gmail.com>
2025-01-12 23:11:18 +0100

    refactoring get-document endpoint

--- routes/auth.js
+++ routes/auth.js
@@ -22,11 +22,11 @@ router.get('/' , getAllUsers);
 router.post('/register' , register);
 router.post('/login' , login);
 router.get('/getUserProfile' , getUserProfile);
-router.put('/update-profile-picture', authMiddleware, uploadMiddleware.single('file'),updateProfilePicture);
+router.put('/update-profile-picture', authMiddleware, uploadMiddleware.single('file'), updateProfilePicture);
 router.post('/select-category', selectCategory);
 router.patch('/upgrade-category', upgradeCategory);
 router.get('/get-user-category-list', getUserCategoryList)
 router.delete('/delete-user', authMiddleware, deleteUser);
-router.post('/logout' , logoutUser)
+router.post('/logout' , logoutUser);
 
 module.exports = router;
